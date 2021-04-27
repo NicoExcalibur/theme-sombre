817df2ae90cf91f2ed54982a06826c6448a01f8f
NicoEcxalibur <nico.zanz@gmail.com>
2021-04-27 16:49:34 +0200

    button works

--- exemple.js
+++ exemple.js
@@ -1,5 +1,17 @@
-// programme
+// Je selectionne les éléments 
+let button = document.querySelector('#mode');
+let span = document.querySelector('span');
 
+// fonction mode sombre
+function darkMode(){
+  // classe du body sur dark
+  // change le texte du bouton en thème clair
+  // localStorage theme = sombre
+  console.log('dark mode');
+}
+
+
+// programme
 if (localStorage.getItem('theme')) {
   // valeur = sombre ?
     // oui : on apelle le mode sombre
@@ -8,17 +20,19 @@ if (localStorage.getItem('theme')) {
   // rien
 }
 
-// click
+// detection du clic
+button.addEventListener('click', () => {
+  // body classe dark ?
+  if (document.body.classList.contains('dark')) {
+    // oui : retire les classes de body / change le texte du bouton / localStorage theme = clair
+    document.body.classList.remove();
+    button.textContent('Thème clair');
+    localStorage.setItem('theme', 'light');
+  }else {
+    // non : appele la fonction darkMode()
+    darkMode();
+  }
+});
 
-// body classe dark ?
-  // oui : retire les classes de body / change le texte du bouton / localStorage theme = clair
-  // non : appele la fonction darkMode()
 
-// fonction mode sombre
-
-function darkMode(){
-  // classe du body sur dark
-  // change le texte du bouton en thème clair
-  // localStorage theme = sombre
-}
 
