52ce3f18410ee9b223bc72f4f61bb7caddb87a74
NicoEcxalibur <nico.zanz@gmail.com>
2021-04-27 17:02:28 +0200

    switch mode with button works

--- exemple.js
+++ exemple.js
@@ -5,9 +5,12 @@ let span = document.querySelector('span');
 // fonction mode sombre
 function darkMode(){
   // classe du body sur dark
+  document.body.classList.add('dark');
   // change le texte du bouton en thème clair
+  span.textContent = 'Thème clair';
   // localStorage theme = sombre
-  console.log('dark mode');
+  localStorage.setItem('theme', 'dark');
+  
 }
 
 
@@ -25,8 +28,8 @@ button.addEventListener('click', () => {
   // body classe dark ?
   if (document.body.classList.contains('dark')) {
     // oui : retire les classes de body / change le texte du bouton / localStorage theme = clair
-    document.body.classList.remove();
-    button.textContent('Thème clair');
+    document.body.classList.remove('dark');
+    span.textContent = 'Thème sombre';
     localStorage.setItem('theme', 'light');
   }else {
     // non : appele la fonction darkMode()
