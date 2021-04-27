f895dc05a67456f87ce374ec33a8e20301eebbc3
NicoEcxalibur <nico.zanz@gmail.com>
2021-04-27 16:38:51 +0200

    select elements

---
+++ exemple.js
@@ -0,0 +1,24 @@
+// programme
+
+if (localStorage.getItem('theme')) {
+  // valeur = sombre ?
+    // oui : on apelle le mode sombre
+    // non : rien
+} else {
+  // rien
+}
+
+// click
+
+// body classe dark ?
+  // oui : retire les classes de body / change le texte du bouton / localStorage theme = clair
+  // non : appele la fonction darkMode()
+
+// fonction mode sombre
+
+function darkMode(){
+  // classe du body sur dark
+  // change le texte du bouton en thème clair
+  // localStorage theme = sombre
+}
+
